462bd1f8adecaa02748cef78105d2e5857d05057
Achraf El Kadiri <ashrafelkadiri@hotmail.com>
2025-02-22 11:41:30 +0100

    modificaciones

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LanguageContext } from "../context/LanguageContext";
-import { IntlProvider} from "react-intl";
+import { IntlProvider } from "react-intl";
 import App from "../App";
 
 
@@ -8,7 +8,7 @@ const Root = () => {
     const { locale, messages } = React.useContext(LanguageContext);
     return (
         <IntlProvider locale={locale} messages={messages}>
-            <App />
+            {<App />}
         </IntlProvider>
     );
 };
